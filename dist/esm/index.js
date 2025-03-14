01d8e7ec5cee1b5ec9d007dc3915df919e5a2c7a
James Chartrand <jc.chartrand@gmail.com>
2025-03-14 11:45:30 -0400

    remove dist

--- dist/esm/index.js
+++
@@ -1,6 +0,0 @@
-import cjsModule from '../index.js';
-export const createFromMultibase = cjsModule.createFromMultibase;
-export const driver = cjsModule.driver;
-export const DidWebDriver = cjsModule.DidWebDriver;
-export const didUrlToHttpsUrl = cjsModule.didUrlToHttpsUrl;
-export const httpsUrlToDidUrl = cjsModule.httpsUrlToDidUrl;
